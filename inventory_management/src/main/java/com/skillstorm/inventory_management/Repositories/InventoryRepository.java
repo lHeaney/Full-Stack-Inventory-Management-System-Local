8485f5ade8a914cd75651eaf504afb1f62c6b677
lHeaney <liamheaneypers@gmail.com>
2025-11-13 16:09:53 -0500

    Confirmed Database Login
    spring boot application runs and connects to database with username and password.
    
    empty class files have been created for initial API setups but are completely blank at this stage. Not all files have been created
    
    *keeping username and password in the .yaml file would not be done in an actual application. It was be extracted from another file that is not shared to any other repository

---
+++ inventory_management/src/main/java/com/skillstorm/inventory_management/Repositories/InventoryRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package com.skillstorm.inventory_management.Repositories;
+
+/**
+ *
+ * @author firef
+ */
+public interface InventoryRepository {
+
+}
